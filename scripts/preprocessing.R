dee81b88c4b701733fbd6e9967e912bc09f9460e
Martijn van Beest <martijn.vanbeest@student.uva.nl>
2016-05-25 21:07:01 +0200

    cleanup

--- scripts/preprocessing.R
+++ scripts/preprocessing.R
@@ -23,7 +23,7 @@ sapply(universities, class)
 trim = function (x) gsub("^\\s+|\\s+$", "", x)
 
 ### variable: income ###
-# Convert income to numeric
+# convert income to numeric
 universities$income=as.numeric(universities$income)
 # Fill missing income values with median
 hist(universities$income)
@@ -35,7 +35,7 @@ universities$income[is.na(universities$income)] =
 universities$international_students = 
   as.numeric(substr(universities$international_students,1,
                     nchar(universities$international_students)-1))/100
-# Fill missing values with median
+# fill missing values with median
 hist(universities$international_students,prob=TRUE)
 universities$international_students[is.na(universities$international_students)] = 
   median(universities$international_students,na.rm=TRUE)
@@ -49,7 +49,7 @@ for (i in 1:length(universities$female_male_ratio))
   universities$f_m_ratio[i] = round(tmp[1]/tmp[2], 2)
 }
 
-# Fill missing female/male ratios with average of mean and median (close together)
+# fill missing female/male ratios with average of mean and median (close together)
 hist(universities$f_m_ratio)
 length(universities$f_m_ratio[is.na(universities$f_m_ratio)])/
   length(universities$f_m_ratio)
@@ -90,17 +90,19 @@ universities$nr_students[is.na(universities$nr_students)] = round(median(univers
 universities$num_students = universities$nr_students
 universities = universities[ , -which(names(universities) %in% c("nr_students"))]
 
-# Fill missing student staff ratios with median
+### variable: student_staff_ratio ###
+# fill missing student staff ratios with median
 hist(universities$student_staff_ratio,prob=TRUE)
 universities$student_staff_ratio[is.na(universities$student_staff_ratio)] = 
   median(universities$student_staff_ratio,na.rm=TRUE)
 
-# Create id column to identify university
+# create id column to identify university
 universities$id = 1:200
 
-# Move descriptive columns to seperate dataframe
+# move descriptive columns to seperate dataframe
 university_info = universities[c(14,2,3)]
 universities = universities[-c(2,3)]
 
+# write cleaned data set to csv
 write.csv(universities, file="csv/universities-cleaned.csv", row.names=FALSE)
 write.csv(university_info, file="csv/universities-info.csv", row.names=FALSE)
